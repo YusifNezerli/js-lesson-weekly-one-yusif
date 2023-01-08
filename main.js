a65d8ad20492458534c2b16afdcecec639e399a6
YusifNezerli <nezerliyusif42@mail.ru>
2023-01-08 20:09:05 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+alert("Wecome")
+
+let b = prompt ("Name")
+
+let a = prompt ("How old are you?")
+
+if (a>18){
+alert("welcome " + b )
+}
+
+else{
+    alert("18 yasdan asagi")
+    open("./index.html","_self")
+}
